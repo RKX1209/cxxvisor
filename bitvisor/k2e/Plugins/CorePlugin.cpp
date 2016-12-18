37f901a47061cc0c8e4c4dee30129d0048bda571
rkx1209 <rkx1209dev@gmail.com>
2016-12-18 16:29:44 +0900

    Add test stub of k2e_on_module_start

--- bitvisor/k2e/Plugins/CorePlugin.cpp
+++ bitvisor/k2e/Plugins/CorePlugin.cpp
@@ -1,6 +1,7 @@
 #include "CorePlugin.hpp"
 extern "C" {
 #include <core/printf.h>
+#include <core/types.h>
 }
 #include <k2e/K2E.hpp>
 
@@ -23,6 +24,12 @@ void k2e_on_module_load(K2E* k2e, struct ModuleDescriptor *module)
   params.ml.md = module;
   //printf("k2e(%p)->getCorePlugin()(%p) %llx(%llx)\n", k2e, k2e->getCorePlugin(), params.ml.md->LoadVBase, params.ml.md->LoadPBase);
   k2e->getCorePlugin()->onModuleLoad.emit(&params);
+}
+
+void k2e_on_module_start(K2E* k2e, u64 pc)
+{
+  K2ECallbackParams params;
+  
 }
 
 }
